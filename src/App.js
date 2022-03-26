a2c72505898c1c3ec11cdfcf5f576c1020a0926e
Ishwar Choudhary <ichoudhary@infoblox.com>
2022-03-26 11:05:00 +0530

    chore: added boiler plate code for the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { color } from '@mui/system';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='header_1'>
+        <div class="d-flex justify-content-between">
+          <div class="header_content_1">ishwar x Strava</div>
+          <div class="header_content_2">
+            <a class="email_href" href=''>email</a>
+            </div>
+        </div>
+      </div>
+
+      <div className='header_2'>
+
+      </div>
+
+
+      <div className='body_content_1 body_content_all'>
+        about me
+      </div>
+
+      <div className='body_content_2 body_content_all'>
+        education & experience
+      </div>
+
+      <div className='body_content_3 body_content_all'>
+        technical "prowess"
+      </div>
+
+      <div className='body_content_4 body_content_all'>
+        why hire me? 
+      </div>
+
+      <div className='body_content_5 body_content_all'>
+        my Strava 
+      </div>
+
     </div>
   );
 }
