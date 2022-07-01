38be46edf32a0d9f7d473adf4477927a39e93c59
Ishwar Choudhary <ichoudhary@infoblox.com>
2022-07-01 13:11:45 +0530

    chore: changed background of education with ssu as well

--- src/pages/education/education.js
+++ src/pages/education/education.js
@@ -5,27 +5,37 @@ import React, {useState} from 'react';
 import Header from '../../common/header/header';
 import Navbar from '../../common/navbar/navbar';
 
-import background from '../../assets/squiggly5.png';
+import background from '../../assets/squiggly6_greeen.png';
+import ssu from '../../assets/ssu.jpg';
+
 
 var information= 'Graduated with GPA of 8.74/10';
+var isInformationVisible = false;
 
 function Education(props) {
   const [,setState] = useState();
 
   function changeInfo(data) {
+
     if(data === 'btech'){
-      information = 'Graduated with GPA of 8.74/10';
+      information = 'Graduated with GPA of 8.74/10, and worked on multiple genres of projects. During this time, I fell in love with the field of Cloud Computing and full stack product development.';
+      isInformationVisible = true;
     }
     else if (data === 'ms'){
-      information = 'Beginning my studies this Fall as a full-time student at Boulder';
+      information = 'Beginning my studies this Fall as a full-time student at Boulder. My goal is to specialise in the field of cloud computing and backend systems.';
+      isInformationVisible = true;
     }
     else if (data === 'internship'){
-      information = 'Worked as a full stack developer with technologies such as Python, .NET & VueJs';
+      information = 'Worked as a Full Stack Developer, incorporating technologies such as Python, .NET, mySQL & VueJs. Developed and deployed two customer facing projects. The more successful of the two handles live traffic of more than 300 systems. ';
+      isInformationVisible = true;
     }
     else if (data === 'fulltime'){
-      information = 'Working as a front end developer with Angular uFEs';
+      information = 'Worked as a front end developer for more than a year with Angular micro-front end. Implemented customer facing features for their BloxOne DDI product, developed UI components, bug-fixing and also participated in UX designing.';
+      isInformationVisible = true;
     }
-    else information = 'Hover over the experience to view more information';
+    else {
+      isInformationVisible = false;
+    }
 
     setState({});
    }
@@ -34,7 +44,7 @@ function Education(props) {
     <div className="Education">
       <Header/>
       <div className='bodycontent' style={{ backgroundImage:`url(${background})`,
-        backgroundRepeat: 'no-repeat',
+        backgroundRepeat: 'repeat',
         backgroundSize:'100vw'}}>     
       
       <div className='d-flex justify-content-start'>
@@ -58,7 +68,7 @@ function Education(props) {
             <br></br>
             <b>Software Developer ['21-'22]</b></span>
         </div>
-        <div className='information_box'>
+        <div className={'information_box' + (isInformationVisible ? '' : ' information_box_invisible')}>
         <span className='highlight_block_information'>{information}</span>
             <br></br>
         </div>
@@ -67,6 +77,7 @@ function Education(props) {
 
         </div> 
 
+      {/* <img className="ssu" src={ssu} /> */}
       <Navbar/>
     </div>
   );
