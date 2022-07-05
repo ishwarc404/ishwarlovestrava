0d4fa1b7d154815323eb47bee43bdf752b1f37e0
Ishwar Choudhary <ichoudhary@infoblox.com>
2022-07-05 07:50:50 +0530

    chore: added info visible persistence to education page as well

--- src/pages/education/education.js
+++ src/pages/education/education.js
@@ -12,6 +12,7 @@ import Discussion from '../../common/discussion/discussion';
 
 var information = 'Graduated with GPA of 8.74/10';
 var isInformationVisible = false;
+var pathValue  = 1;
 
 function Education(props) {
   const [, setState] = useState();
@@ -21,22 +22,26 @@ function Education(props) {
     if (data === 'btech') {
       information = 'Graduated with GPA of 8.74/10, and worked on multiple genres of projects. During this time, I fell in love with the field of Cloud Computing and full stack product development.';
       isInformationVisible = true;
+      pathValue  = 1;
     }
     else if (data === 'ms') {
       information = 'Beginning my studies this Fall as a full-time student at Boulder. My goal is to specialise in the field of cloud computing and backend systems.';
       isInformationVisible = true;
+      pathValue  = 2;
     }
     else if (data === 'internship') {
       information = 'Worked as a Full Stack Developer, incorporating technologies such as Python, .NET, mySQL & VueJs. Developed and deployed two customer facing projects. The more successful of the two handles live traffic of more than 300 systems. ';
       isInformationVisible = true;
+      pathValue  = 3;
     }
     else if (data === 'fulltime') {
       information = 'Worked as a front end developer for more than a year with Angular micro-front end. Implemented customer facing features for their BloxOne DDI product, developed UI components, bug-fixing and also participated in UX designing.';
       isInformationVisible = true;
+      pathValue  = 4;
     }
-    else {
-      isInformationVisible = false;
-    }
+    // else {
+    //   isInformationVisible = false;
+    // }
 
     setState({});
   }
@@ -53,22 +58,22 @@ function Education(props) {
 
           <div className='d-flex justify-content-start'>
             <div className='header_2_education'>
-              <span onMouseOver={() => changeInfo('btech')} onMouseLeave={() => changeInfo('')} className='highlight_block_education'>Bachelor's in Computer Science
+              <span onMouseOver={() => changeInfo('btech')} className={'highlight_block_education' + (pathValue === 1 ? ' educationhighlight_container_1' : '')}>Bachelor's in Computer Science
                 <br></br>
                 PES University, Bangalore ['21]</span>
               <br></br>
               <br></br>
-              <span onMouseOver={() => changeInfo('ms')} onMouseLeave={() => changeInfo('')} className='highlight_block_education'>Master's in Computer Science
+              <span onMouseOver={() => changeInfo('ms')}  className={'highlight_block_education'+ (pathValue === 2 ? ' educationhighlight_container_2' : '')}>Master's in Computer Science
                 <br></br>
                 University of Colorado, Boulder  ['24]</span>
               <br></br>
               <br></br>
-              <span onMouseOver={() => changeInfo('internship')} onMouseLeave={() => changeInfo('')} className='highlight_block_education'>Bot Mantra, DeltaX
+              <span onMouseOver={() => changeInfo('internship')}  className={'highlight_block_education'+ (pathValue === 3 ? ' educationhighlight_container_3' : '')}>Bot Mantra & DeltaX
                 <br></br>
                 Software Developer Intern ['19-'21]</span>
               <br></br>
               <br></br>
-              <span onMouseOver={() => changeInfo('fulltime')} onMouseLeave={() => changeInfo('')} className='highlight_block_education'>Infoblox Inc.
+              <span onMouseOver={() => changeInfo('fulltime')}  className={'highlight_block_education'+ (pathValue === 4 ? ' educationhighlight_container_4' : '')}>Infoblox Inc.
                 <br></br>
                 Software Developer ['21-'22]</span>
             </div>
