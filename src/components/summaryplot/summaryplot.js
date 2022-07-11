aff7d851f6a566f6a04c8024c79958306783969c
Ishwar Choudhary <ichoudhary@infoblox.com>
2022-07-11 08:15:25 +0530

    chore: imp - added summary plot

---
+++ src/components/summaryplot/summaryplot.js
@@ -0,0 +1,82 @@
+import './summaryplot.css';
+
+import ishwarname from '../../assets/ishwar.png';
+import xletter from '../../assets/x.png';
+import strava from '../../assets/b&w.png';
+
+import React, { useState, useEffect } from 'react';
+
+
+var mileageData = null
+var mileageDataComponent = [];
+var strokeComponentPoints = ""
+function SummaryPlot(props) {
+  const [, setState] = useState();
+
+
+  useEffect(() => {
+    var mileageData = props.mileageData;
+    var x_value = 40;
+    mileageDataComponent = [];
+    strokeComponentPoints = [];
+    for (var i = 0; i < mileageData.miles.length; i++) {
+      mileageDataComponent.push(
+        <circle className="quiz-graph-start-dot" cx={x_value} cy={100-mileageData.miles[i]} key={x_value} data-value="7.2" r="6"></circle>
+      )
+      strokeComponentPoints = strokeComponentPoints + `${x_value},${100-mileageData.miles[i]} `;
+      console.log(strokeComponentPoints);
+      x_value += 40;
+      
+    }
+    // setState({});
+  })
+
+  return (
+    <div className='SummaryPlot'>
+      <div className="">
+
+        <div>
+         
+          <svg className="quiz-graph">
+            <defs>
+              <pattern id="grid" width="40" height="10" patternUnits="userSpaceOnUse">
+                <path d="M 0 0 L 0 0 0 50" fill="none" stroke="#cfcfcf" stroke-width="1"></path>
+              </pattern>
+            </defs>
+            <rect x="40" width="500px" height="150px" fill="url(#grid)" stroke="gray"></rect>
+            <g className="label-title">
+            </g>
+
+            <linearGradient id="grad" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" style={{stopColor:'#fc5200',stopOpacity:1}}></stop>
+              <stop offset="100%" style={{stopColor:'white',stopOpacity:'0'}}></stop>
+            </linearGradient>
+            {/* <polyline fill="url(#grad)" stroke="#fc5200" stroke-width="0" points="
+            50,300
+            51,0
+            150,100
+            250,80
+            350,160
+            450,100
+            550,100
+            650,150
+            750,200 
+            750,300
+            "></polyline>
+
+          */}
+            <polyline fill="none" stroke="#fc5200" stroke-width="2" points={strokeComponentPoints}></polyline> 
+          
+            <g>
+{mileageDataComponent}
+            </g>
+          </svg>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default SummaryPlot;
--- src/pages/mystrava/mystrava.js
+++ src/pages/mystrava/mystrava.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import Header from '../../common/header/header';
 import Navbar from '../../common/navbar/navbar';
 import Discussion from '../../common/discussion/discussion';
+import SummaryPlot from '../../components/summaryplot/summaryplot';
 
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
@@ -19,6 +20,7 @@ import total_activities_hand from '../../assets/total_activities_hand.png';
 import total_hours_hand from '../../assets/total_hours_hand.png';
 import latest_activity_hand from '../../assets/latest_activity_hand.png';
 import kudos_received_hand from '../../assets/kudos_received_hand.png';
+import weekly_summary_hand from '../../assets/weekly_summary_hand.png'
 
 
 var athleteStatsData = {};
@@ -37,7 +39,7 @@ var latestActivity = {
 }
 var latestActivityId = 0;
 
-const maxActivityPages = 3; //change this to 5
+const maxActivityPages = 0; //change this to 5
 const baseURL = "https://www.strava.com/api/v3/athletes/43290018/stats";
 const singleActivityURL = "https://www.strava.com/api/v3/activities/"
 const refreshToken = 'bd8b400a40d972c7e45c69720e41a47f8e661597';
@@ -46,6 +48,12 @@ var accessToken = '814ab41c4c149fdcc6ff3e22941e1ca2948fd1a4'
 var imageCount = 0
 var displayImage = imgs[imageCount]
 var latestActivityFlag = true;
+
+var mileageData = {
+  maxMileage: 100,
+  miles: [20, 30, 30, 25, 15, 20, 28, 35]
+}
+
 function convertSeconds(value) {
   const sec = parseInt(value, 10); // convert value to number if it's string
   let hours = Math.floor(sec / 3600); // get hours
@@ -74,7 +82,7 @@ function Mystrava() {
       // console.log(response.data);
       accessToken = response.data['access_token']
 
-      for (var i = 0; i < 3; i++) {
+      for (var i = 0; i < 1; i++) {
         // console.log('i is ',i);
         //ALL ACTIVITY DATA
         axios.get(`https://www.strava.com/api/v3/athlete/activities?before=1657381150&page=${i + 1}&per_page=200`, {
@@ -135,16 +143,16 @@ function Mystrava() {
     //   setState({});
     // });
 
-    setInterval(function () {
-      if (imageCount > 24) {
-        imageCount = 0;
-      }
-      imageCount++
-      console.log(imageCount);
-      displayImage = imgs[imageCount];
-      console.log(displayImage);
-      setState({});
-    }, 4000);
+    // setInterval(function () {
+    //   if (imageCount > 24) {
+    //     imageCount = 0;
+    //   }
+    //   imageCount++
+    //   console.log(imageCount);
+    //   displayImage = imgs[imageCount];
+    //   console.log(displayImage);
+    //   setState({});
+    // }, 4000);
 
 
   }, []);
@@ -170,7 +178,7 @@ function Mystrava() {
           {/* </div> */}
 
 
-          <div className='d-flex'>
+          <div className='d-flex justify-content-center'>
             <div className=''>
               <div className='information-div'>
                 <img className="i1" src={total_activities_hand} />
@@ -180,6 +188,7 @@ function Mystrava() {
                 <img className="i2" src={total_hours_hand} />
                 <div className='information-div-child'>{Math.round(totalHoursTillDate / 3600)}</div>
               </div>
+
             </div>
             {/* <div className='d2'>
             <div></div>
@@ -187,28 +196,39 @@ function Mystrava() {
             <div>
               <div className='information-div'><img className="i3" src={latest_activity_hand} />
                 <div>
-                <div className='information-div-child-lots'>
-                  <div><b>{latestActivity.name}</b></div>
-                  <div>{latestActivity.description}</div>
-                  <div>{convertSeconds(latestActivity.elapsed_time)} </div>
-                  <div><i>{latestActivity.sport_type}</i>
-                    {latestActivity.sport_type == 'Run' ? (<img className=" activity_icon run" src={runimage} />) :
-                      latestActivity.sport_type == 'Ride' ? (<img className=" activity_icon bike" src={bikeimage} />) :
-                        latestActivity.sport_type == 'Swim' ? (<img className=" activity_icon swim" src={swimimage} />) :
-                          latestActivity.sport_type == 'WeightTraining' ? (<img className=" activity_icon weighttraining" src={weightimage} />) :
-                            <span></span>
-                    }
+                  <div className='information-div-child-lots'>
+                    <div><b>{latestActivity.name}</b></div>
+                    <div>{latestActivity.description}</div>
+                    <div>{convertSeconds(latestActivity.elapsed_time)} </div>
+                    <div><i>{latestActivity.sport_type}</i>
+                      {latestActivity.sport_type == 'Run' ? (<img className=" activity_icon run" src={runimage} />) :
+                        latestActivity.sport_type == 'Ride' ? (<img className=" activity_icon bike" src={bikeimage} />) :
+                          latestActivity.sport_type == 'Swim' ? (<img className=" activity_icon swim" src={swimimage} />) :
+                            latestActivity.sport_type == 'WeightTraining' ? (<img className=" activity_icon weighttraining" src={weightimage} />) :
+                              <span></span>
+                      }
+                    </div>
+                    <div className='grid-item-3-value-count'>
+                      <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" data-testid="unfilled_kudos"><path d="M15.243 7.182a1.907 1.907 0 00-.532-1.423 2.069 2.069 0 00-1.493-.641H9.863l.454-1.812A2.426 2.426 0 008.064.514h-.513l-.718 2.807L4.97 6.915.412 9.34l2.472 6.424 4.278-2.28h4.785a2.142 2.142 0 002.127-1.976l.084-1.177a1.962 1.962 0 00.712-2.097 1.93 1.93 0 00.373-1.052zM1.664 9.807l2.06-1.1 1.748 4.542-2.061 1.1-1.747-4.542zm12.289-2.038l-.268.254.165.331a.942.942 0 01-.044.903.965.965 0 01-.369.352l-.237.131-.122 1.7a1.123 1.123 0 01-1.129 1.049H6.914l-.552.295-1.748-4.547 1.1-.586 2.033-3.92.567-2.166a1.427 1.427 0 011.032 1.371c0 .071 0 .139-.007.167l-.758 3.016h4.64a1.059 1.059 0 01.764.328.917.917 0 01.26.683.942.942 0 01-.292.639z" fill=""></path></svg>
+                      {latestActivity.kudos_count}
+                    </div>
                   </div>
-                  <div className='grid-item-3-value-count'>
-                    <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" data-testid="unfilled_kudos"><path d="M15.243 7.182a1.907 1.907 0 00-.532-1.423 2.069 2.069 0 00-1.493-.641H9.863l.454-1.812A2.426 2.426 0 008.064.514h-.513l-.718 2.807L4.97 6.915.412 9.34l2.472 6.424 4.278-2.28h4.785a2.142 2.142 0 002.127-1.976l.084-1.177a1.962 1.962 0 00.712-2.097 1.93 1.93 0 00.373-1.052zM1.664 9.807l2.06-1.1 1.748 4.542-2.061 1.1-1.747-4.542zm12.289-2.038l-.268.254.165.331a.942.942 0 01-.044.903.965.965 0 01-.369.352l-.237.131-.122 1.7a1.123 1.123 0 01-1.129 1.049H6.914l-.552.295-1.748-4.547 1.1-.586 2.033-3.92.567-2.166a1.427 1.427 0 011.032 1.371c0 .071 0 .139-.007.167l-.758 3.016h4.64a1.059 1.059 0 01.764.328.917.917 0 01.26.683.942.942 0 01-.292.639z" fill=""></path></svg>                  
-                    {latestActivity.kudos_count}
-                  </div>
-                </div>
                 </div>
               </div>
               <div className='information-div'><img className="i4" src={kudos_received_hand} />
                 <div className='information-div-child-kudos'>{userKudosRecievedCount}</div>
               </div>
+            </div>
+            <div className='information-div-weekly-summary'>
+                <img className="i1" src={weekly_summary_hand} />
+                <div className='activity-summary-button-suite'>
+                  <button className='activity-summary-button'><img className="activity_icon run" src={runimage}/>Run</button>
+                  <button className='activity-summary-button'><img className=" activity_icon bike" src={bikeimage}/>Ride</button>
+                  <button className='activity-summary-button'><img className=" activity_icon swim" src={swimimage}/>Swim</button>
+                  <button className='activity-summary-button'>
+                  <img className=" activity_icon weighttraining" src={weightimage} />Weight Training</button>
+                </div>
+                <SummaryPlot mileageData={mileageData} />
             </div>
           </div>
 
