38a2a7ce577cb83e78333bc212e1190dcfd8da12
Ishwar Choudhary <ishwarchoudhary@Ishwars-MacBook-Air.local>
2022-07-29 09:50:30 -0700

    chore: fixed kudos logic

--- src/common/discussion/discussion.js
+++ src/common/discussion/discussion.js
@@ -8,7 +8,7 @@ import axios from "axios";
 var userData = {
   time: '',
   comment: ''
-} 
+}
 
 var kudos_counter = 0;
 var user_ip_ = null;
@@ -20,70 +20,90 @@ function Discussion(props) {
 
 
   useEffect(() => {
-
-    // axios.get('https://geolocation-db.com/json/')
-    // .then(response => {
-    //   user_ip  = response.data.IPv4;
-    //   setState({});
-    // })
     const get_ip = async () => {
       console.log("got here");
       const response = await fetch("https://geolocation-db.com/json/");
       const data = await response.json();
-      // console.log(data);
-      // user_ip = data.IPv4;
-      axios.get('https://sheet.best/api/sheets/a41c21aa-1c83-4b4f-a478-549345179b89')
-      .then(response => {
-        kudos_counter = response.data.length;
-        var user_ip = data.IPv4;
-        user_ip_ = user_ip;
-        // console.log(user_ip_);
-        for(var i=0 ;i<response.data.lengths_counter;i++){
-          console.log(response.data[i]['ip']);
-          if(response.data[i]['ip'] === user_ip){
+      user_ip_ = data.IPv4;
+      setState({});
 
-            kudos_given_before = true;
+      axios.get('https://sheet.best/api/sheets/f03e4f53-e301-449b-b2ea-743bef0a5e23')
+        .then(response => {
+          kudos_counter = response.data.length;
+          setState({});
+          for (var i = 0; i < response.data.length; i++) {
+            if (response.data[i]['ip'] === data.IPv4) {
+              kudos_given_before = true;
+              setState({});
+            }
           }
-        }
-        setState({});
-      })
+        })
+
+
     };
-    
+
     get_ip();
-      // user_ip  = get_ip().IPv4;
-      // setState({});
 
 
 
-
-  })
+  }, [])
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log('comment:', comment);
     axios.post('https://sheet.best/api/sheets/20663903-9d53-43a3-beea-43cb136f12f3', {
-    date: Date.now(),  
-    comment:comment})
-    .then(response => {
-      console.log(response);
+      date: Date.now(),
+      comment: comment
     })
-    
+      .then(response => {
+        console.log(response);
+      })
+
   }
 
   function handleKudosSubmit(event) {
-    console.log(user_ip_);
-    event.preventDefault();
-    if(!kudos_given_before){
 
-    axios.post('https://sheet.best/api/sheets/a41c21aa-1c83-4b4f-a478-549345179b89', {
-    ip:user_ip_,
-    date: Date.now(),  
-    kudos:1})
-    .then(response => {
-      console.log(response);
-    })
-          
+    // axios.get('https://sheet.best/api/sheets/f03e4f53-e301-449b-b2ea-743bef0a5e23')
+    //   .then(response => {
+    //     console.log('kudo handler!');
+    //     // console.log(response.data);
+    //     kudos_counter = response.data.length;
+    //     for (var i = 0; i < response.data.length; i++) {
+    //       // console.log(response.data[i]['ip']);
+    //       if (response.data[i]['ip'] === user_ip_) {
+    //         console.log('here', user_ip_);
+    //         console.log('here', response.data[i]);
+    //         kudos_given_before = true;
+    //       }
+    //       else {
+
+    //       }
+    //     }
+    if (!kudos_given_before) {
+      console.log('there');
+      axios.post('https://sheet.best/api/sheets/f03e4f53-e301-449b-b2ea-743bef0a5e23', {
+        ip: user_ip_,
+        date: Date.now(),
+        kudos: 1
+      })
+        .then(response => {
+          kudos_counter +=1 ;
+          kudos_given_before = true;
+          setState({});
+        })
+
     }
+
+    setState({});
+      // })
+
+    // console.log(user_ip_);
+    event.preventDefault();
+    // if(!kudos_given_before){
+
+
+
+    // }
   }
 
 
@@ -99,16 +119,16 @@ function Discussion(props) {
 
       </div>
       <form onSubmit={handleSubmit} >
-      <div className='d-flex justify-content-center'>
-        <div className='d-flex justify-content-around write-comment'>
-          <input className='write-comment-input-box' placeholder="Add a comment. Share your Strava!"  
-           value={comment}
-           onChange={(e) => {setComment(e.target.value);}} />
+        <div className='d-flex justify-content-center'>
+          <div className='d-flex justify-content-around write-comment'>
+            <input className='write-comment-input-box' placeholder="Add a comment. Share your Strava!"
+              value={comment}
+              onChange={(e) => { setComment(e.target.value); }} />
+          </div>
+          <div className='d-flex justify-content-around post-comment'>
+            <button className="postButton" type='submit'><div className='write-comment-post-button'><b>Post</b></div></button>
+          </div>
         </div>
-        <div className='d-flex justify-content-around post-comment'>
-        <button className="postButton" type='submit'><div className='write-comment-post-button'><b>Post</b></div></button>
-        </div>
-      </div>
       </form>
     </div>
 
