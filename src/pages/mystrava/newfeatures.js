5bcca10df92cab21ad3ec853f070bc91d79f7f04
ishwarc404 <ishwarc404@gmail.com>
2022-08-27 12:35:52 -0600

    chore: added new feature

--- src/pages/mystrava/newfeatures.js
+++ src/pages/mystrava/newfeatures.js
@@ -4,6 +4,7 @@ import iphone from '../../assets/iphone.png';
 import translate1 from '../../assets/translate1.jpg';
 import translate2 from '../../assets/translate2.png';
 import arrow_left from '../../assets/arrow_left.png';
+import kilianjgroupride from '../../assets/kilianjgroupride.png';
 
 var translate1Flag = true;
 
@@ -37,7 +38,26 @@ function NewFeatures() {
             </div>
           </div>
         </div>
+      
       </div>
+      <div className='d-flex justfity-content-center feature-2'>
+          {/* <div className='prototypes-title'>Translate</div> */}
+          <div className='prototypes'>
+            <img className='iphone-2-prototype' src={iphone} width="300"></img>
+            <img className={'iphone-2-image-original'} src={kilianjgroupride}></img>
+            {/* <img className={'iphone-1-image-translate' + (translate1Flag ? ' invisible' : '')} src={translate2}></img> */}
+            {/* <button onClick={()=>{translate1Flag = false; setState({});}} className={'iphone-1-image-translate-button' + (translate1Flag ? '' : ' invisible')}>Translate</button> */}
+            {/* <button onClick={()=>{translate1Flag = true; setState({});}} className={'iphone-1-image-original-button' + (translate1Flag ? ' invisible' : '')}>Original</button> */}
+          </div>
+          <div className='feature-2-info'>
+            <div className='feature-2-info-title'>
+            GROUP RIDES WITH LIVE LOCATION.
+            </div>
+            <div className='feature-2-info-title'>
+            So that if you get lost, you can easily re-group. Either ways, you can't keep up with Kilian Jornet. 🐐
+            </div>
+          </div>
+        </div>
 
     </div>
 
