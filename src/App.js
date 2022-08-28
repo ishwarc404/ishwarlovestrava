b70faad062c83819627a28bfb0180d69cac8caff
ishwarc404 <ishwarc404@gmail.com>
2022-08-28 15:57:46 -0600

    chore: replaced browserrouter with hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
 import Home from './pages/home/home';
