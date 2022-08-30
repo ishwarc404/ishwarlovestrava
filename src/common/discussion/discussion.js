a41d0a75807a0c78df857a13b9a1b89a8eb82bb2
ishwarc404 <ishwarc404@gmail.com>
2022-08-30 06:49:38 -0600

    chore: enabled API

--- src/common/discussion/discussion.js
+++ src/common/discussion/discussion.js
@@ -32,7 +32,7 @@ function Discussion(props) {
       setState({});
 
       //getting kudos
-      axios.get('https://sheet.best/api/sheets/a24b66d3-4748-416a-ab2e-04364b0a71adX')
+      axios.get('https://sheet.best/api/sheets/a24b66d3-4748-416a-ab2e-04364b0a71ad')
         .then(response => {
           kudos_counter = response.data.length;
           setState({});
@@ -45,7 +45,7 @@ function Discussion(props) {
         })
 
         //getting comments
-        axios.get('https://sheet.best/api/sheets/543df280-5e35-4d92-b872-3624e364a312X')
+        axios.get('https://sheet.best/api/sheets/543df280-5e35-4d92-b872-3624e364a312')
         .then(response => {
           userComments = []
           userComments2 = []
@@ -77,7 +77,7 @@ function Discussion(props) {
   function handleSubmit(event) {
     event.preventDefault();
     if (comment.length != 0 || comment != '' || comment != null || comment.length > 200) {
-      axios.post('https://sheet.best/api/sheets/543df280-5e35-4d92-b872-3624e364a312X', {
+      axios.post('https://sheet.best/api/sheets/543df280-5e35-4d92-b872-3624e364a312', {
         date: Date.now(),
         comment: comment
       })
@@ -97,7 +97,7 @@ function Discussion(props) {
   function handleKudosSubmit(event) {
 
     if (!kudos_given_before) {
-      axios.post('https://sheet.best/api/sheets/a24b66d3-4748-416a-ab2e-04364b0a71adX', {
+      axios.post('https://sheet.best/api/sheets/a24b66d3-4748-416a-ab2e-04364b0a71ad', {
         ip: user_ip_,
         date: Date.now(),
         kudos: 1
