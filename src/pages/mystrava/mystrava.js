ff5b80524a65164d698d47f51f53b10bda55926f
Ishwar Choudhary <ishwarc404@gmail.com>
2023-02-16 17:43:05 -0700

    chore: updated API refresh key

--- src/pages/mystrava/mystrava.js
+++ src/pages/mystrava/mystrava.js
@@ -56,8 +56,8 @@ const maxActivityPages = 5; //change this to 3 or 0
 const baseURL = "https://www.strava.com/api/v3/athletes/43290018/stats";
 const athleteDataURL = "https://www.strava.com/api/v3/athlete";
 const singleActivityURL = "https://www.strava.com/api/v3/activities/"
-const refreshToken = '4e66afee4416c157ba4452861edcf50c7674d629';
-const refreshURL = 'https://www.strava.com/oauth/token?client_id=89361&client_secret=453c72ddb9de476feab5816537fbd884184b7ced&refresh_token=4e66afee4416c157ba4452861edcf50c7674d629&grant_type=refresh_token'
+const refreshToken = '94eb1a960acb3e3a584037f2edb954b59e5319ca';
+const refreshURL = 'https://www.strava.com/oauth/token?client_id=89361&client_secret=453c72ddb9de476feab5816537fbd884184b7ced&refresh_token=94eb1a960acb3e3a584037f2edb954b59e5319ca&grant_type=refresh_token'
 var accessToken = '814ab41c4c149fdcc6ff3e22941e1ca2948fd1a4'
 var imageCount = 0
 var displayImage = imgs[imageCount]
